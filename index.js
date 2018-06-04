2d5dd8e377b8df93e97a59998be93962fc932d79
Adrian S <adrian.stoichita@gmail.com>
2018-06-04 21:34:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import a library to help create a component
+import React, {Component} from 'react';
+import {AppRegistry, View} from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+class App extends Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Header headerText={'Albums'}/>
+                <AlbumList/>
+            </View>
+        );
+    }
+}
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
